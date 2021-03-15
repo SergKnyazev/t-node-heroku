55ea703849e8439660936518849a7790a008bd27
SergKnyazev <59279467+SergKnyazev@users.noreply.github.com>
2021-03-15 16:51:31 +0300

    change server.js

--- server.js
+++ server.js
@@ -67,7 +67,8 @@ const app = express();
 app.use(cors());
 
 // app.use(express.static(path.resolve(__dirname, './public')));
-app.use(express.static(path.resolve(__dirname)));
+app.use(express.static(path.resolve(__dirname, './build/static')));
+// app.use(express.static(path.resolve(__dirname)));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
